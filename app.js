ee62232288d1b769f6d96d9950a2e953e4257556
Raphael Henrique <raphaelhcc321@gmail.com>
2021-05-27 01:20:51 -0300

    update cors

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(morgan('dev'));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
-        'Access-Control-Allow-Header',
+        'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     );
     res.header('Access-Control-Allow-Credentials', 'true');
