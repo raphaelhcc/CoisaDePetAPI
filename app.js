286c341828173864334602e520bfa006a05ab188
raphael <raphaelhcc321@gmail.com>
2021-05-30 22:15:58 -0300

    update cors

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header(
         'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Headers, Access-Control-Max-Age'
     );
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
